39fe2f6a1353fd45b0571d893c5055012743614c
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2024-07-27 22:29:52 +0000

    Add function which checks entered data

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,4 +32,39 @@ function letsCalculate(desiredSavings, salary, rentMortgage, bills, food, otherE
 
 }
 
+/**
+ * Check entered data by a user
+ * If data are correct - the code is going through calculation
+ */
+
+function checkData() {
+    let desiredSavings = parseInt(document.getElementById("desired-saving").value);
+    let salary = parseInt(document.getElementById("salary").value);
+    let rentMortgage = parseInt(document.getElementById("rent-mortgage").value);
+    let bills = parseInt(document.getElementById("bills").value);
+    let food = parseInt(document.getElementById("food").value);
+    let otherExpenses = parseInt(document.getElementById("other-expenses").value);
+    if (desiredSavings < 1) {
+        resultText.innerText = `You entered desired saving: ${desiredSavings}. The value cannot be lees than 1. Please eneter another value and try again!`;
+        resultText.style.color = "red";
+    } else if (salary < 1) {
+        resultText.innerText = `You entered salary: ${salary}. The value cannot be lees than 1. Please eneter another value and try again!`;
+        resultText.style.color = "red";
+    } else if (rentMortgage < 0) {
+        resultText.innerText = `You entered rent/mortgage expenses: ${rentMortgage}. The value cannot be lees than 0. Please eneter another value and try again!`;
+        resultText.style.color = "red";
+    } else if (bills < 0) {
+        resultText.innerText = `You entered bills expenses: ${bills}. The value cannot be lees than 0. Please eneter another value and try again!`;
+        resultText.style.color = "red";
+    } else if (food < 0) {
+        resultText.innerText = `You entered food expenses: ${food}. The value cannot be lees than 0. Please eneter another value and try again!`;
+        resultText.style.color = "red";
+    } else if (otherExpenses < 0) {
+        resultText.innerText = `You entered salary: ${otherExpenses}. The value cannot be lees than 0. Please eneter another value and try again!`;
+        resultText.style.color = "red";
+    } else {
+        letsCalculate(desiredSavings, salary, rentMortgage, bills, food, otherExpenses);
+    }
+}
+
 
